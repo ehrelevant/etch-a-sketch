e367cdc41873cc5400bf0925a2d78bbf37f528bc
KindaExists <ehrencastillo@gmail.com>
2021-08-08 22:39:49 +0800

    added optional color functions

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 const container = document.querySelector('#container');
 container.addEventListener('mouseenter', startTrail, true);
 
-generateGrid(container, 100)
+generateGrid(container, 16)
 
 function startTrail(e) {
     if(e.target.className === 'box') {
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor = `rgb(${getRandomValue()}, ${getRandomValue()}, ${getRandomValue()})`;
+        e.target.style.opacity = parseFloat(window.getComputedStyle(e.target).opacity) - 0.1;
     }
 }
+
+function getRandomValue() {
+    return Math.floor(Math.random() * 256);
+}
+
 
 function generateGrid(container, squaresAtSide) {
     const gridLength = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--grid-length'));
@@ -28,3 +34,11 @@ function generateGrid(container, squaresAtSide) {
         }
     }
 }
+
+
+const btn = document.querySelector('#reset');
+btn.addEventListener('click', (e)=>{
+    squaresAtSide = prompt('Number of squares per side of Grid?')
+    container.textContent = ''
+    generateGrid(container, squaresAtSide)
+});
