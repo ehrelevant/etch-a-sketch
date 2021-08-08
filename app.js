6ec91e600882e4fd8622607ff3ed0171d1d9342a
KindaExists <ehrencastillo@gmail.com>
2021-08-08 19:19:02 +0800

    added 16x16 grid generation code

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
 const container = document.querySelector('#container');
-for(let y = 0; y < 16; y++) {
-    for(let x = 0; x < 16; x++) {
-        const box = document.createElement('div');
-        box.classList.add('box')
-        container.appendChild(box);
-        console.log(x);
+
+generateGrid(container, 16)
+
+function startTrail(e) {
+    e.target.style.backgroundColor = 'black'
+}
+
+function generateGrid(container, squaresAtSide) {
+    for(let y = 0; y < squaresAtSide; y++) {
+        for(let x = 0; x < squaresAtSide; x++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+
+            box.setAttribute('data-x', x.toString());
+            box.setAttribute('data-y', y.toString());
+
+            box.addEventListener('mouseenter', startTrail);
+            container.appendChild(box);
+        }
     }
 }
