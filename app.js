3f151058f15e38e193eace634070205edfc9600e
KindaExists <ehrencastillo@gmail.com>
2021-08-08 22:57:58 +0800

    cleaned loop code

--- app.js
+++ app.js
@@ -22,16 +22,10 @@ function generateGrid(container, squaresAtSide) {
     document.documentElement.style.setProperty('--square-length', `${gridLength / squaresAtSide}px`);
     document.documentElement.style.setProperty('--squares-in-grid', squaresAtSide.toString());
 
-    for(let y = 0; y < squaresAtSide; y++) {
-        for(let x = 0; x < squaresAtSide; x++) {
-            const box = document.createElement('div');
-            box.classList.add('box');
-
-            // box.setAttribute('data-x', x.toString());
-            // box.setAttribute('data-y', y.toString());
-
-            container.appendChild(box);
-        }
+    for(let y = 0; y < squaresAtSide * squaresAtSide; y++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        container.appendChild(box);
     }
 }
 
