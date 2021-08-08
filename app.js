b21fc4e1f3d031de7cb4a80c3b180cc618afbb3f
KindaExists <ehrencastillo@gmail.com>
2021-08-08 19:40:41 +0800

    added grid-changing functions

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function startTrail(e) {
 }
 
 function generateGrid(container, squaresAtSide) {
+    const gridLength = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--grid-length'));
+
+    document.documentElement.style.setProperty('--square-length', `${gridLength / squaresAtSide}px`);
+    document.documentElement.style.setProperty('--squares-in-grid', squaresAtSide.toString());
+
     for(let y = 0; y < squaresAtSide; y++) {
         for(let x = 0; x < squaresAtSide; x++) {
             const box = document.createElement('div');
